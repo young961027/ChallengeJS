408f02076032ca9cf72340d93487f89101447ed1
loopbackseal <young961027@gmail.com>
2021-01-16 14:45:03 +0900

    save country to LS by change event

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 // <⚠️ DONT DELETE THIS ⚠️>
 // import "styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
-const clock = document.querySelector("#js-clock");
-function getTime() {
-  // Don't delete this.
-  let now = new Date();
-  const xmasDay = new Date("2021-12-24:00:00:00+0900");
-  let gap = Math.floor((xmasDay - now) / 1000);
-  const daysToX = Math.floor(gap / 86400);
-  gap = gap - daysToX * 86400;
-  const hoursToX = Math.floor(gap / 3600);
-  gap = gap - hoursToX * 3600;
-  const minutesToX = Math.floor(gap / 60);
-  gap = gap - minutesToX * 60;
-  clock.innerText = `${daysToX < 10 ? `0${daysToX}` : daysToX}d ${
-    hoursToX < 10 ? `0${hoursToX}` : hoursToX
-  }h ${minutesToX < 10 ? `0${minutesToX}` : minutesToX}m ${
-    gap < 10 ? `0${gap}` : gap
-  }s`;
+const country = document.querySelector(".Country");
+const greeting = document.querySelector(".js-greeting");
+
+function saveCountry(text) {
+  localStorage.setItem("country", text);
+}
+
+function paintGreeting(country) {
+  if (country === "KR") {
+    greeting.innerText = "안녕하세요!";
+  } else if (country === "GR") {
+    greeting.innerText = "Καλημέρα!";
+  } else if (country === "TK") {
+    greeting.innerText = "Günaydın!";
+  } else if (country === "FL") {
+    greeting.innerText = "Hyvää huomenta!";
+  }
+}
+
+function handleChange(event) {
+  const currentValue = event.target.value;
+  saveCountry(currentValue);
+  paintGreeting(currentValue);
 }
 
 function init() {
-  setInterval(getTime, 1000);
+  country.addEventListener("change", handleChange);
 }
+
 init();
