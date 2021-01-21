fb23f9799805cb17a43257180d3c2355510a3ebe
loopbackseal <young961027@gmail.com>
2021-01-21 18:49:26 +0900

    Random number game

--- index.js
+++ index.js
@@ -1,153 +1,31 @@
-const PEND_LS = "PENDING";
-const FIN_LS = "FINISHED";
-const form = document.querySelector(".js-form"),
-  input = document.querySelector(".js-input"),
-  pendList = document.querySelector(".js-pending"),
-  finList = document.querySelector(".js-finished");
-let toDoPending = [];
-let toDoFinished = [];
-let idNum = 1;
-let finNum = 1;
+const range = document.querySelector(".js-range");
+const notice = document.querySelector(".js-notice");
+const input = document.querySelector(".js-input");
+const button = document.querySelector(".js-button");
+const span = document.querySelector(".js-span");
+const result = document.querySelector(".js-result");
 
-function savePending() {
-  localStorage.setItem(PEND_LS, JSON.stringify(toDoPending));
-}
-
-function saveFinished() {
-  localStorage.setItem(FIN_LS, JSON.stringify(toDoFinished));
-}
-
-function handleDelFin(event) {
-  const li = event.target.parentNode;
-  const cleanFinished = toDoFinished.filter(function (toDo) {
-    return toDo.id !== parseInt(li.id, 10);
-  });
-  while (finList.firstChild) {
-    finList.removeChild(finList.firstChild);
+function handleClick(event) {
+  const value = input.value;
+  if (value !== "") {
+    const answer = Math.floor(Math.random() * range.value);
+    span.innerText = `You chose : ${value}, the machine chose : ${answer}`;
+    if (parseInt(value) === answer) {
+      result.innerText = "YOU WIN!";
+    } else {
+      result.innerText = "YOU LOST!";
+    }
   }
-  finNum = 1;
-  cleanFinished.forEach(function (toDo) {
-    toDo.id = finNum;
-    paintFinished(toDo.text);
-  });
-  toDoFinished = cleanFinished;
-  saveFinished();
 }
 
-function handleDelPend(event) {
-  const li = event.target.parentNode;
-  const cleanPending = toDoPending.filter(function (toDo) {
-    return toDo.id !== parseInt(li.id, 10);
-  });
-  while (pendList.firstChild) {
-    pendList.removeChild(pendList.firstChild);
-  }
-  idNum = 1;
-  cleanPending.forEach(function (toDo) {
-    toDo.id = idNum;
-    paintPending(toDo.text);
-  });
-  toDoPending = cleanPending;
-  savePending();
-}
-
-function handleFin(event) {
-  const li = event.target.parentNode;
-  const currentValue = toDoPending.filter(function(toDo) {
-      return toDo.id === parseInt(li.id, 10);
-  });
-  currentValue.forEach(function(toDo) {
-      paintFinished(toDo.text);
-  })
-  handleDelPend(event);
-}
-  
-function handleRtn(event) {
-    const li = event.target.parentNode;
-    const currentValue = toDoFinished.filter(function(toDo) {
-        return toDo.id === parseInt(li.id, 10);
-    });
-    currentValue.forEach(function(toDo) {
-        paintPending(toDo.text);
-    })
-    handleDelFin(event);
-}
-
-function paintFinished(text) {
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  const rtnBtn = document.createElement("button");
-  const span = document.createElement("span");
-  delBtn.innerText = "❌";
-  delBtn.addEventListener("click", handleDelFin);
-  rtnBtn.innerText = "⏮";
-  rtnBtn.addEventListener("click", handleRtn);
-  span.innerText = text;
-  li.appendChild(span);
-  li.appendChild(delBtn);
-  li.appendChild(rtnBtn);
-  li.id = finNum;
-  finList.appendChild(li);
-  const finObj = {
-    text,
-    id: finNum
-  };
-  toDoFinished.push(finObj);
-  finNum += 1;
-  saveFinished();
-}
-
-function paintPending(text) {
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  const finBtn = document.createElement("button");
-  const span = document.createElement("span");
-  delBtn.innerText = "❌";
-  delBtn.addEventListener("click", handleDelPend);
-  finBtn.innerText = "✅";
-  finBtn.addEventListener("click", handleFin);
-  span.innerText = text;
-  li.appendChild(span);
-  li.appendChild(delBtn);
-  li.appendChild(finBtn);
-  li.id = idNum;
-  pendList.appendChild(li);
-  const pendObj = {
-    text,
-    id: idNum
-  };
-  toDoPending.push(pendObj);
-  idNum += 1;
-  savePending();
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const currentValue = input.value;
-  paintPending(currentValue);
-  input.value = "";
-}
-
-function loadToDos() {
-  const loadedPending = localStorage.getItem(PEND_LS);
-  const loadedFinished = localStorage.getItem(FIN_LS);
-  if (loadedPending !== null) {
-    const parsedPending = JSON.parse(loadedPending);
-    parsedPending.forEach(function (toDo) {
-      paintPending(toDo.text);
-    });
-  }
-  if (loadedFinished !== null) {
-    const parsedFinished = JSON.parse(loadedFinished);
-    parsedFinished.forEach(function (toDo) {
-      paintFinished(toDo.text);
-    });
-  }
+function handleInput(event) {
+  const value = event.target.value;
+  notice.innerText = `Generate a Number between 0 and ${value}`;
 }
 
 function init() {
-  loadToDos();
-  form.addEventListener("submit", handleSubmit);
+  range.addEventListener("input", handleInput);
+  button.addEventListener("click", handleClick);
 }
 
 init();
